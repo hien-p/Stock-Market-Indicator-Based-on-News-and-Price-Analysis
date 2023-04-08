c8edbdfe633e34e25095265a7b8e60e395854ac9
h4438 <www.hao2912@gmail.com>
2023-04-08 17:26:07 +0700

    backend to the current point

---
+++ Backend-Web/clone-git.sh
@@ -0,0 +1,24 @@
+rmdir -R data
+echo "Delete data"
+USER="h4438"
+EMAIL="www.hao2912@gmail.com"
+TOKEN="7d5a95fcb968cb2697edf3cc170dcc576e29a6d2"
+REPO_NAME="Stock-Indicator-News"
+REPO="https://"$USER":"$TOKEN"@dagshub.com/h4438/"$REPO_NAME".git"
+DVC_REPO="https://dagshub.com/"$USER"/"$REPO_NAME".dvc"
+mkdir data
+cd data
+pwd
+git clone $REPO
+cd $REPO_NAME
+git pull origin master
+echo "Git cloned"
+pwd
+git config --global user.email $EMAIL
+git config --global user.name $USER
+dvc remote default origin --local
+dvc remote modify origin --local auth basic
+dvc remote modify origin --local user $USER
+dvc remote modify origin --local password $TOKEN
+echo "Press any key to exit"
+read
